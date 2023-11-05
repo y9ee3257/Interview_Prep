c4e307ab237277d1b7e6dd2ced8ccb5e26395789
y9ee3257 <rt.usapp@gmail.com>
2023-11-05 18:06:44 -0500

    grokking the coding interview - searching

---
+++ ravi/searching/grok_modified_binary_search/first_bad_version.py
@@ -0,0 +1,41 @@
+# The latest version of a software product fails the quality check. Since each version is developed upon the previous
+# one, all the versions created after a bad version are also considered bad.
+#
+# Suppose you have n versions with the IDs [1,2,...,n]
+# , and you have access to an API function that returns TRUE if the argument is the ID of a bad version.
+#
+# Find the first bad version that is causing all the later ones to be bad. Additionally, the solution should also
+# return the number of API calls made during the process and should minimize the number of API calls too.
+# Input 100
+# Output [67, 7]
+#
+# Input 13
+# Output [10, 3]
+
+from api import *
+
+version_api = api(0)
+
+
+def is_bad_version(v):
+    return version_api.is_bad(v)
+
+
+def first_bad_version(n):
+    # -- DO NOT CHANGE THIS SECTION
+    version_api.n = n
+    # --
+
+    left = 1
+    right = n
+    first_bad_version = n
+    api_calls = 0
+    while left < right:
+        mid = (left + right) // 2
+        if is_bad_version(mid):
+            first_bad_version = min(first_bad_version, mid)
+            right = mid
+        else:
+            left = mid + 1
+        api_calls += 1
+    return first_bad_version, api_calls
---
+++ ravi/searching/grok_modified_binary_search/rotated_binary_search.py
@@ -0,0 +1,35 @@
+# Given a sorted integer array, nums, and an integer value, target, the array is rotated by some arbitrary number. Search and return the index of target in this array. If the target does not exist, return -1.
+#
+# An original sorted array before rotation is given below:
+#
+# After rotating this array 6 times, it changes to:
+#
+# Constraints
+#
+# All values in nums are unique.
+# The values in nums are sorted in ascending order.
+# The array may have been rotated by some arbitrary number.
+
+
+def rotated_binary_search(array, target):
+    return rotated_binary_search_rec(array, target, 0, len(array) - 1)
+
+
+def rotated_binary_search_rec(array, target, start, end):
+    if start > end:
+        return -1
+    mid = (start + end) // 2
+    if target == array[mid]:
+        return mid
+    elif target < array[start] and target <= array[end]:
+        return rotated_binary_search_rec(array, target, mid + 1, end)
+    else:
+        return rotated_binary_search_rec(array, target, start, mid - 1)
+
+
+print(rotated_binary_search([4, 5, 6, 1, 2, 3], 1))
+print(rotated_binary_search([4, 5, 6, 1, 2, 3], 2))
+print(rotated_binary_search([4, 5, 6, 1, 2, 3], 3))
+print(rotated_binary_search([4, 5, 6, 1, 2, 3], 4))
+print(rotated_binary_search([4, 5, 6, 1, 2, 3], 5))
+print(rotated_binary_search([4, 5, 6, 1, 2, 3], 6))
